0c6d6f1ce3bbc77fd0ddf64c8557d5e8c847ba22
LisaDaVinchie <lisadavinchie@gmail.com>
2025-03-07 17:58:31 +0100

    Added data import and paths handling

--- src/train.py
+++ src/train.py
@@ -23,24 +23,23 @@ paths_path = args.paths
 results_path: Path = None
 figs_path: Path = None
 weights_path: Path = None
-paths = load_config(paths_path, ["results"])
+processed_data_dir: Path = None
+masks_dir: Path = None
+
+n_channels: int = None
+paths = load_config(paths_path, ["data", "results"])
 locals().update(paths["results"])
+locals().update(paths["data"])
 
 results_path = Path(results_path)
 figs_path = Path(figs_path)
 weights_path = Path(weights_path)
+processed_data_dir = Path(processed_data_dir)
+masks_dir = Path(masks_dir)
 
-if not results_path.parent.exists():
-    print("Results folder does not exist.", flush = True)
-    raise FileNotFoundError(f"Results folder does not exist: {results_path.parent}")
-
-if not figs_path.parent.exists():
-    print("Figures folder does not exist.", flush = True)
-    raise FileNotFoundError(f"Figures folder does not exist: {figs_path.parent}")
-
-if not weights_path.parent.exists():
-    print("Weights folder does not exist.", flush = True)
-    raise FileNotFoundError(f"Weights folder does not exist: {weights_path.parent}")
+for path in [results_path.parent, figs_path.parent, weights_path.parent, processed_data_dir, masks_dir]:
+    if not path.exists():
+        raise FileNotFoundError(f"Path {path} does not exist.")
 
 # Load the train parameters
 train_perc: int = None
@@ -51,8 +50,11 @@ optimizer_kind: str = None
 loss_kind: str = None
 model_kind: str = None
 placeholder: float = None
-config = load_config(params_path, ["training"])
+
+n_channels: int = None
+config = load_config(params_path, ["training", "dataset"])
 locals().update(config["training"])
+locals().update(config["dataset"])
 
 if model_kind == "simple_conv":
     model = simple_conv(params_path)
@@ -66,18 +68,23 @@ else:
     raise ValueError(f"Model kind {model_kind} not recognized")
 
 # Import dataset
-dataset = th.rand(100, 13, 10, 10)
-channels_to_mask = [3, 4, 5]
-masks = th.ones(100, 13, 10, 10)
+# dataset = th.rand(100, 13, 10, 10)
+# channels_to_mask = [3, 4, 5]
+# masks = th.ones(100, 13, 10, 10)
 
-masks[:, channels_to_mask, :2, :2] = 0
+# masks[:, channels_to_mask, :2, :2] = 0
 
+dataset_start_time = time()
+data_files = list(processed_data_dir.glob("*.pt"))
+mask_files = list(masks_dir.glob("*.pt"))
 
-    
+dataset = th.stack([th.load(file) for file in data_files])
+masks = th.load(masks_dir / f"mask_n{len(data_files)}_c{n_channels}.pt")
 
-print(f"Dataset shape: {dataset.shape}, Masks shape: {masks.shape}\n")
+print(f"Dataset created in {time() - dataset_start_time:.2f} seconds")
 
 train_loader, test_loader = create_dataloaders(dataset, masks, train_perc, batch_size)
+del dataset, masks
 
 loss_function = th.nn.MSELoss()
 optimizer = th.optim.Adam(model.parameters(), lr=learning_rate)
