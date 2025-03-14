dfeceff303ff5ae04a9862abc6ba75c3812e8a0f
LisaDaVinchie <lisadavinchie@gmail.com>
2025-03-14 17:17:53 +0100

    Improved path handling and introduced txt explanatory file

--- src/cut_images.py
+++ src/cut_images.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import argparse
 from time import time
 from utils.import_params_json import load_config
-from mask_data import mask_inversemask_image
+from mask_data import mask_inversemask_image, create_square_mask
 import random
+import json
 
 import matplotlib.pyplot as plt
 
@@ -21,9 +22,8 @@ params_path = args.params
 paths_path = args.paths
 
 processed_data_dir: Path = None
-cutted_images_dir: Path = None
-cutted_images_basename: str = None
-cutted_images_file_ext: str = None
+cutted_images_path: Path = None
+cutted_txt_path: Path = None
 paths = load_config(paths_path, ["data", "results"])
 locals().update(paths["data"])
 
@@ -34,18 +34,16 @@ n_cutted_images = None
 cutted_width = None
 cutted_height = None
 n_channels = None
-params = load_config(params_path, ["dataset"])
+mask_percentage = None
+params = load_config(params_path, ["dataset", "mask"])
 locals().update(params["dataset"])
+locals().update(params["mask"])
 
 
 processed_data_dir = Path(processed_data_dir)
-cutted_images_dir = Path(cutted_images_dir)
 
 if not processed_data_dir.exists():
     raise FileNotFoundError(f"Path {processed_data_dir} does not exist.")
-
-if not cutted_images_dir.exists():
-    raise FileNotFoundError(f"Path {cutted_images_dir} does not exist.")
 
 # Select n_images random images from the processed images
 processed_images_paths = list(processed_data_dir.glob(f"*.pt"))
@@ -53,7 +51,7 @@ processed_images_paths = list(processed_data_dir.glob(f"*.pt"))
 if len(processed_images_paths) == 0:
     raise FileNotFoundError(f"No images found in {processed_data_dir}")
 
-print(f"Found {len(processed_images_paths)} images in {processed_data_dir}")
+print(f"\nFound {len(processed_images_paths)} images in {processed_data_dir}\n", flush=True)
 
 
 # Select some random points, to use as centers for the cutted images
@@ -72,24 +70,7 @@ for point in random_points:
     
     path_to_indices[path].append(point)
     
-print(f"Selected random points for cutted images in {time() - idx_time} seconds")
-
-def create_square_mask(image_width: int, image_height: int, mask_percentage: float) -> th.tensor:
-    """Create a square mask of n_pixels in the image"""
-    n_pixels = int(mask_percentage * image_width * image_height)
-    square_width = int(n_pixels ** 0.5)
-    mask = th.ones((image_width, image_height), dtype=th.int)
-    
-    # Get a random top-left corner for the square
-    row_idx = th.randint(0, image_height - square_width, (1,)).item()
-    col_idx = th.randint(0, image_width - square_width, (1,)).item()
-    
-    mask[
-        row_idx: row_idx + square_width,
-        col_idx: col_idx + square_width
-    ] = 0
-    
-    return mask
+print(f"Selected random points for cutted images in {time() - idx_time} seconds\n", flush=True)
 
 d_time = time()     
 keys = ["masked_images", "inverse_masked_images", "masks"]
@@ -101,21 +82,36 @@ for path, indices in path_to_indices.items():
     
     for index in indices:
         cutted_img = image[:, index[0]:index[0] + cutted_width, index[1]:index[1] + cutted_height].unsqueeze(0)
-        masks = th.stack([create_square_mask(cutted_width, cutted_height, 0.05).unsqueeze(0) for _ in range(n_channels)], dim=1)
+        masks = th.stack([create_square_mask(cutted_width, cutted_height, mask_percentage).unsqueeze(0) for _ in range(n_channels)], dim=1)
         dataset["masked_images"][idx], dataset["inverse_masked_images"][idx] = mask_inversemask_image(cutted_img, masks, 0)
         dataset["masks"][idx] = masks
         idx += 1
 
 
-print(f"Cutted images in {time() - d_time} seconds")
+print(f"Cutted images in {time() - d_time} seconds\n", flush=True)
 
 # Save the cutted images
 save_time = time()
-
-cutted_images_path = cutted_images_dir / f"{cutted_images_basename}_n{n_cutted_images}_c{n_channels}{cutted_images_file_ext}"
 th.save(dataset, cutted_images_path)
 
-print("Elapsed time: {:.2f} seconds".format(time() - start_time))
+with open(params_path, 'r') as json_file:
+    data = json.load(json_file)
+
+# Extract the "dataset" and "mask" sections
+dataset_section = data.get('dataset', {})
+mask_section = data.get('mask', {})
+
+# Combine the sections into a single dictionary
+sections_to_save = {
+    'dataset': dataset_section,
+    'mask': mask_section
+}
+
+# Save the combined sections to a text file
+with open(cutted_txt_path, 'w') as txt_file:
+    json.dump(sections_to_save, txt_file, indent=4)
+
+print("Elapsed time: {:.2f} seconds\n".format(time() - start_time), flush=True)
 
 
 
