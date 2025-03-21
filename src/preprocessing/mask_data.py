ed8cd71e084fb976148e95cad589104f95ce6766
LisaDaVinchie <lisadavinchie@gmail.com>
2025-03-21 16:50:18 +0100

    Removed non used lines

--- src/preprocessing/mask_data.py
+++ src/preprocessing/mask_data.py
@@ -70,56 +70,24 @@ def create_square_mask(image_width: int, image_height: int, mask_percentage: flo
     
     return mask
 
-class SquareMask():
-    def __init__(self, params_path: Path, image_width: int = None, image_height: int = None, mask_percentage: float = None):
-        """Create a square mask of n_pixels in the image"""
-        
-        params = load_config(params_path, ["dataset"]).get("dataset", {})
-        self.image_width = image_width if image_width is not None else params.get("image_width", 10)
-        self.image_height = image_height if image_height is not None else params.get("image_height", 10)
-        
-        params = load_config(params_path, ["square_mask"]).get("square_mask", {})
-        self.mask_percentage = mask_percentage if mask_percentage is not None else params.get("mask_percentage", 10)
-        
-
-        n_pixels = int(self.mask_percentage * self.image_width * self.image_height)
-        square_width = int(n_pixels ** 0.5)
-        self.half_square_width = square_width // 2
+# def create_lines_mask(image_width: int, image_height: int, min_thickness: int, max_thickness: int, n_lines: int) -> th.tensor:
+#     """Create a mask with lines of random thickness"""
     
-    def mask(self) -> th.tensor:
-        """Mask the image with a square"""
-        mask = th.ones((self.image_width, self.image_height), dtype=th.int)
-        center_row = th.randint(self.half_square_width, self.image_height - self.half_square_width, (1,)).item()
-        center_col = th.randint(self.half_square_width, self.image_width - self.half_square_width, (1,)).item()
+#     mask = th.ones((image_width, image_height), dtype=th.float32)
+    
+#     for i in range(n_lines):
+#         # Get random x locations to start line
+#         x1, x2 = th.randint(1, image_width, (2,))
+#         # Get random y locations to start line
+#         y1, y2 = th.randint(1, image_height, (2,))
+#         # Get random thickness of the line drawn
+#         thickness = th.randint(min_thickness, max_thickness, (1,)).item()
+#         # Draw black line on the white mask
+#         mask[
+#             x1: x2,
+#             y1: y2
+#         ] = 0
         
-        mask[
-            max(0, center_row - self.half_square_width): min(self.image_height, center_row + self.half_square_width),
-            max(0, center_col - self.half_square_width): min(self.image_width, center_col + self.half_square_width)
-        ] = 0
+#         cv2.line(mask.numpy(), (x1, y1), (x2, y2), (0, 0, 0), thickness)
         
-        return mask
-        
-
-# class LineMask:
-#     def __init__(self, image_width: int = None, image_height: int = None, min_thickness: int = None, max_thickness: int = None, n_lines: int = None):
-#         self.image_width = image_width
-#         self.image_height = image_height
-#         self.max_tichkness = max_thickness
-#         self.min_thickness = min_thickness
-#         self.n_lines = n_lines
-
-#     def create_mask(self) -> np.ndarray:
-#         ## Prepare masking matrix
-        
-#         mask = np.zeros((self.image_height, self.image_width), dtype=np.uint8)
-#         for _ in range(np.random.randint(1, self.n_lines)):
-#             # Get random x locations to start line
-#             x1, x2 = np.random.randint(1, self.image_width), np.random.randint(1, self.image_width)
-#             # Get random y locations to start line
-#             y1, y2 = np.random.randint(1, self.image_height), np.random.randint(1, self.image_height)
-#             # Get random thickness of the line drawn
-#             thickness = np.random.randint(self.min_thickness, self.max_tichkness)
-#             # Draw black line on the white mask
-#             cv2.line(mask,(x1,y1),(x2,y2),(1,1,1),thickness)
-
-#         return mask.astype(bool)
+#     return th.tensor(mask)
