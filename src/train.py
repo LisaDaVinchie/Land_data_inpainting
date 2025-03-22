4fb431973522f7d2090304032cc0bbc4c5640bb7
LisaDaVinchie <lisadavinchie@gmail.com>
2025-03-22 16:24:47 +0100

    Created new training loop

---
+++ src/train.py
@@ -0,0 +1,238 @@
+import torch as th
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from pathlib import Path
+import argparse
+from time import time
+import json
+import psutil
+import os
+
+from CustomDataset import create_dataloaders
+from models import initialize_model_and_dataset_kind
+from preprocessing.mask_data import mask_inversemask_image
+from losses import per_pixel_loss, per_pixel_mse
+
+def main():
+    parser = argparse.ArgumentParser(description='Train a CNN model on a dataset')
+    parser.add_argument('--paths', type=Path, help='Path to the JSON file with data paths')
+    parser.add_argument('--params', type=Path, help='Path to the JSON file with model parameters')
+    parser.add_argument('--dataset_idx', type=int, help='Index of the dataset to use', required=False)
+
+    args = parser.parse_args()
+    
+    start_time = time()
+
+    params_path = args.params
+    paths_path = args.paths
+    
+    with open(params_path, 'r') as f:
+        params = json.load(f)
+    
+    train_perc = params["training"]["train_perc"]
+    epochs = params["training"]["epochs"]
+    batch_size = params["training"]["batch_size"]
+    learning_rate = params["training"]["learning_rate"]
+    optimizer_kind = params["training"]["optimizer_kind"]
+    loss_kind = params["training"]["loss_kind"]
+    model_kind = params["training"]["model_kind"]
+    placeholder = params["training"]["placeholder"]
+    print("Parameters imported\n", flush = True)
+    
+    with open(paths_path, 'r') as f:
+        paths = json.load(f)
+    
+    results_path = Path(paths["results"]["results_path"])
+    weights_path = Path(paths["results"]["weights_path"])
+    current_extended_dataset_path = Path(paths["data"]["current_extended_dataset_path"])
+    current_minimal_dataset_path = Path(paths["data"]["current_minimal_dataset_path"])
+    
+    track_memory("Before loading model")
+    model, dataset_kind = initialize_model_and_dataset_kind(params_path, model_kind)
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    model = model.to(device)
+    track_memory("After loading model")
+    print("Model initialized\n", flush = True)
+    
+    if dataset_kind == "extended":
+        dataset_path = current_extended_dataset_path
+    elif dataset_kind == "minimal":
+        dataset_path = current_minimal_dataset_path
+    else:
+        raise ValueError(f"Dataset kind {dataset_kind} not recognized")
+    
+    idx = args.dataset_idx
+    dataset_path = change_dataset_idx(idx, dataset_path)
+        
+    validate_paths([dataset_path, results_path.parent, weights_path.parent])
+    print("\nPaths imported\n", flush = True)
+
+    dataset_start_time = time()
+    track_memory("Before loading dataset")
+    dataset = th.load(dataset_path)
+    track_memory("After loading dataset")
+    print(f"Dataset created in {time() - dataset_start_time:.2f} seconds", flush = True)
+
+    track_memory("Before creating dataloaders")
+    train_loader, test_loader = create_dataloaders(dataset, train_perc, batch_size)
+    del dataset
+    print("Dataloaders created\n", flush = True)
+    track_memory("After creating dataloaders")
+    
+    if loss_kind == "per_pixel_loss":
+        loss_function = per_pixel_loss
+    elif loss_kind == "per_pixel_mse":
+        loss_function = per_pixel_mse
+    elif loss_kind == "mse":
+        loss_function = nn.MSELoss()
+    else:
+        raise ValueError(f"Loss kind {loss_kind} not recognized")
+    
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    training_time = time()
+    track_memory("Before training")
+    if dataset_kind == "extended":
+        model, train_losses, test_losses = train_loop_extended(epochs, placeholder, model, device, train_loader, test_loader, loss_function, optimizer)
+    elif dataset_kind == "minimal":
+        model, train_losses, test_losses = train_loop_minimal(epochs, placeholder, model, device, train_loader, test_loader, loss_function, optimizer)
+    else:
+        raise ValueError(f"Dataset kind {dataset_kind} not recognized")
+    track_memory("After training")
+    print(f"Training completed in {time() - training_time:.2f} seconds", flush = True)
+
+    # Save the model
+    th.save(model.state_dict(), weights_path)
+
+    with open(params_path, 'r') as f:
+        params = json.load(f)
+        
+    json_str = json.dumps(params, indent=4)[1: -1]
+    
+    elapsed_time = time() - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+    # Save the train losses to a txt file
+    with open(results_path, 'w') as f:
+        f.write("Elapsed time [s]:\n")
+        f.write(f"{elapsed_time}\n\n")
+        f.write("Used dataset:\n")
+        f.write(f"{dataset_path.relative_to(dataset_path.parent.parent)}\n\n")
+        f.write("Train losses\n")
+        for i, loss_function in enumerate(train_losses):
+            f.write(f"{loss_function}\t")
+        f.write("\n\n")
+        f.write("Test losses\n")
+        for i, loss_function in enumerate(test_losses):
+            f.write(f"{loss_function}\t")
+        f.write("\n\n")
+        f.write("Parameters\n")
+        f.write(json_str)
+        
+def track_memory(stage=""):
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info().rss / 1e6  # Convert to MB
+    print(f"[Memory] {stage}: {mem_info:.2f} MB\n", flush=True)
+    
+def change_dataset_idx(dataset_idx: int, dataset_path: Path) -> Path:
+    """Take the latest dataset path by default, or change the dataset index if specified.
+
+    Args:
+        dataset_idx (int): desired dataset index
+        dataset_path (Path): path to the dataset
+
+    Returns:
+        Path: path to the dataset with the desired index
+    """
+    
+    if dataset_idx is None:
+        return dataset_path
+    
+    dataset_basepath = dataset_path.stem.split("_")[0]
+    dataset_ext = dataset_path.suffix
+    dataset_path = dataset_path.parent / f"{dataset_basepath}_{dataset_idx}{dataset_ext}"
+    return dataset_path
+
+def validate_paths(paths: list):
+    """Check if the paths exist.
+
+    Raises:
+        FileNotFoundError: if any of the paths do not exist, raise an error
+    """
+    for path in paths:
+        if not path.exists():
+            raise FileNotFoundError(f"Path {path} does not exist.")
+
+def train_loop_extended(epochs: int, placeholder: float, model: th.nn.Module, device, train_loader: DataLoader, test_loader: DataLoader, loss_function: nn.Module, optimizer: optim.Optimizer):
+    """Training loop for the extended dataset."""
+    train_losses = []
+    test_losses = []
+
+    for epoch in range(epochs):
+        print(f"\nEpoch {epoch + 1}/{epochs}\n", flush=True)
+        model.train()
+        
+        train_loss = 0
+        for i, (masked_image, inverse_masked_image, mask) in enumerate(train_loader):
+            masked_image = masked_image.to(device)
+            inverse_masked_image = inverse_masked_image.to(device)
+            mask = mask.to(device)
+            output = model(masked_image)
+            _, inverse_masked_output = mask_inversemask_image(output, mask, placeholder)
+            loss_val = loss_function(inverse_masked_output, inverse_masked_image) / (th.sum(mask) + 1e-8)
+            train_loss += loss_val.item()
+            
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+        
+        train_losses.append(train_loss / len(train_loader))
+        
+        with th.no_grad():
+            model.eval()
+            test_loss = 0
+            for i, (masked_image, inverse_masked_image, mask) in enumerate(test_loader):
+                output = model(masked_image)
+                _, inverse_masked_output = mask_inversemask_image(output, mask, placeholder)
+                loss_val = loss_function(inverse_masked_output, inverse_masked_image) / (th.sum(mask) + 1e-8)
+                test_loss += loss_val.item()
+            
+            test_losses.append(test_loss / len(test_loader))
+    return model, train_losses, test_losses
+
+def train_loop_minimal(epochs: int, placeholder: float, model: th.nn.Module, device, train_loader: DataLoader, test_loader: DataLoader, loss_function: nn.Module, optimizer: optim.Optimizer):
+    train_losses = []
+    test_losses = []
+
+    for epoch in range(epochs):
+        print(f"\nEpoch {epoch + 1}/{epochs}\n", flush=True)
+        model.train()
+        
+        train_loss = 0
+        for i, (images, masks) in enumerate(train_loader):
+            images = images.to(device)
+            masks = masks.to(device)
+            output, output_masks = model(images, masks)
+            loss_val = loss_function(output, images, masks)
+            train_loss += loss_val.item()
+            
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+        
+        train_losses.append(train_loss / len(train_loader))
+        
+        with th.no_grad():
+            model.eval()
+            test_loss = 0
+            for i, (images, masks) in enumerate(test_loader):
+                output, output_masks = model(images, masks)
+                loss_val = loss_function(output, images, masks)
+                test_loss += loss_val.item()
+            
+            test_losses.append(test_loss / len(test_loader))
+    return model, train_losses, test_losses
+        
+if __name__ == "__main__":
+    main()
