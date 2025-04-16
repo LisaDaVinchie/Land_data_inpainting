70d59a6cd65d180cd069541e9b5a11db00082840
LisaDaVinchie <lisadavinchie@gmail.com>
2025-04-16 15:21:45 +0200

    Refactored per-pixel loss and MSE, added loss selection function

--- src/losses.py
+++ src/losses.py
@@ -1,46 +1,76 @@
 import torch as th
 import torch.nn as nn
 
-def per_pixel_loss(prediction: th.Tensor, target: th.Tensor, masks: th.Tensor) -> th.Tensor:
-    """Calculate the per-pixel loss between the prediction and the target, ignoring masked pixels.
+def get_loss_function(loss_kind: str) -> nn.Module:
+    """Get the loss function based on the specified kind.
 
     Args:
-        prediction (th.Tensor): output of the model, shape (batch_size, channels, height, width)
-        target (th.Tensor): ground truth, shape (batch_size, channels, height, width)
-        masks (th.Tensor): binary mask with 0s for masked pixels, shape (batch_size, channels, height, width)
+        loss_kind (str): type of loss function to use. Options are "per_pixel_loss" or "per_pixel_mse".
 
     Returns:
-        th.Tensor: per-pixel loss
+        nn.Module: loss function
     """
     
-    n_valid_pixels = (~masks.bool()).sum().float()
-    if n_valid_pixels == 0:
-        return th.tensor(0.0, requires_grad=True)
+    if loss_kind == "per_pixel_loss":
+        return PerPixelL1()
+    elif loss_kind == "per_pixel_mse":
+        return PerPixelMSE()
+    elif loss_kind == "total_variation_loss":
+        return TotalVariationLoss()
+    else:
+        raise ValueError(f"Loss kind {loss_kind} not recognized")
     
-    diff = th.abs(prediction - target)
-    masked_diff = diff.masked_fill(masks.bool(), 0.0)
-    return masked_diff.sum() / n_valid_pixels
 
-def per_pixel_mse(prediction: th.Tensor, target: th.Tensor, masks: th.Tensor) -> th.Tensor:
-    """Calculate the per-pixel loss between the prediction and the target on masked pixels.
-
-    Args:
-        prediction (th.Tensor): output of the model, shape (batch_size, channels, height, width)
-        target (th.Tensor): ground truth, shape (batch_size, channels, height, width)
-        masks (th.Tensor): binary mask with 0 where the pixel is masked, shape (batch_size, channels, height, width).
-        The loss is calculated on the masked (0) pixels.
-
-    Returns:
-        th.Tensor: per-pixel loss
-    """
+class PerPixelMSE(nn.Module):
+    def __init__(self):
+        """Initialize the Per Pixel MSE loss module."""
+        super(PerPixelMSE, self).__init__()
     
-    n_valid_pixels = (~masks.bool()).sum().float() # count the number of masked (0) pixels, by inverting the mask
-    if n_valid_pixels == 0: # if all pixels are masked, return 0
-        return 0
+    def forward(self, prediction: th.Tensor, target: th.Tensor, masks: th.Tensor) -> th.Tensor:
+        """Calculate the per-pixel loss between the prediction and the target on masked pixels.
+
+        Args:
+            prediction (th.Tensor): output of the model, shape (batch_size, channels, height, width)
+            target (th.Tensor): ground truth, shape (batch_size, channels, height, width)
+            masks (th.Tensor): binary mask with 0 where the pixel is masked, shape (batch_size, channels, height, width).
+            The loss is calculated on the masked (0) pixels.
+
+        Returns:
+            th.Tensor: per-pixel loss
+        """
+        
+        n_valid_pixels = (~masks.bool()).sum().float() # count the number of masked (0) pixels, by inverting the mask
+        if n_valid_pixels == 0: # if all pixels are masked, return 0
+            return 0
+        
+        diff = (prediction - target) ** 2 # Calculate the squared difference, for each pixel
+        masked_diff = diff.masked_fill(masks.bool(), 0.0) # Set the masked pixels to 0 where the mask is 1, i.e. where the pixel is not masked
+        return masked_diff.sum() / n_valid_pixels # Return the mean of the squared differences over the number of valid pixels
+
+class PerPixelL1(nn.Module):
+    def __init__(self):
+        """Initialize the Per Pixel L1 loss module."""
+        super(PerPixelL1, self).__init__()
     
-    diff = (prediction - target) ** 2 # Calculate the squared difference, for each pixel
-    masked_diff = diff.masked_fill(masks.bool(), 0.0) # Set the masked pixels to 0 where the mask is 1, i.e. where the pixel is not masked
-    return masked_diff.sum() / n_valid_pixels # Return the mean of the squared differences over the number of valid pixels
+    def forward(self, prediction: th.Tensor, target: th.Tensor, masks: th.Tensor) -> th.Tensor:
+        """Calculate the per-pixel loss between the prediction and the target, ignoring masked pixels.
+
+        Args:
+            prediction (th.Tensor): output of the model, shape (batch_size, channels, height, width)
+            target (th.Tensor): ground truth, shape (batch_size, channels, height, width)
+            masks (th.Tensor): binary mask with 0s for masked pixels, shape (batch_size, channels, height, width)
+
+        Returns:
+            th.Tensor: per-pixel loss
+        """
+        
+        n_valid_pixels = (~masks.bool()).sum().float()
+        if n_valid_pixels == 0:
+            return th.tensor(0.0, requires_grad=True)
+        
+        diff = th.abs(prediction - target)
+        masked_diff = diff.masked_fill(masks.bool(), 0.0)
+        return masked_diff.sum() / n_valid_pixels
 
 class TotalVariationLoss(nn.Module):
     def __init__(self):
