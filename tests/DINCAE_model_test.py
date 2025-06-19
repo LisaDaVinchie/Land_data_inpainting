a4efd48aba2e0fc705837b25cda23082150e70dc
LisaDaVinchie <lisadavinchie@gmail.com>
2025-06-19 11:52:44 +0200

    Modified to adapt to new code

--- tests/DINCAE_model_test.py
+++ tests/DINCAE_model_test.py
@@ -17,9 +17,8 @@ class Test_DINCAE_model(unittest.TestCase):
         self.interp_mode = "bilinear"
         self.nrows = 168
         self.ncols = 144 
-        self.channels_to_keep = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9"]
-        self.n_channels = len(self.channels_to_keep) + 1
         self.placeholder = 0.0
+        self.n_channels = 11
         
         self.batch_size = 32
         self.model_params = {
@@ -31,13 +30,12 @@ class Test_DINCAE_model(unittest.TestCase):
                 "cutted_ncols": self.ncols,
                 "dataset_kind": "test",
                 "test": {
-                    "n_channels": self.n_channels,
-                    "channels_to_keep": self.channels_to_keep
+                    "n_channels": self.n_channels
                 },
                 "placeholder": 0.0
             },
             "models": {
-                "DINCAE_like": {
+                "DINCAE": {
                     "middle_channels": self.middle_channels,
                     "kernel_sizes": self.kernel_sizes,
                     "pooling_sizes": self.pooling_sizes,
@@ -49,7 +47,6 @@ class Test_DINCAE_model(unittest.TestCase):
         }
         
         self.model = DINCAE_like(params=self.model_params)
-        self.model.layers_setup()
         
         self.input_tensor = th.rand(self.batch_size, self.model.n_channels, self.nrows, self.ncols)
         self.masks = th.ones(self.batch_size, self.model.n_channels, self.nrows, self.ncols)
@@ -59,31 +56,26 @@ class Test_DINCAE_model(unittest.TestCase):
         """Test if the model initializes correctly"""
         # Test if the model initializes correctly
         
+        model = DINCAE_like(params=self.model_params)
+        
         # Check if the model has the correct attributes
-        self.assertEqual(self.model.n_channels, self.n_channels)
-        self.assertEqual(self.model.middle_channels, self.middle_channels)
-        self.assertEqual(self.model.kernel_sizes, self.kernel_sizes)
-        self.assertEqual(self.model.pooling_sizes, self.pooling_sizes)
-        self.assertEqual(self.model.image_nrows, self.nrows)
-        self.assertEqual(self.model.image_ncols, self.ncols)
-        self.assertEqual(self.model.interp_mode, self.interp_mode)
-        self.assertEqual(self.model.placeholder, self.placeholder)
+        self.assertEqual(model.middle_channels, self.middle_channels)
+        self.assertEqual(model.kernel_sizes, self.kernel_sizes)
+        self.assertEqual(model.pooling_sizes, self.pooling_sizes)
+        self.assertEqual(model.interp_mode, self.interp_mode)
+        self.assertEqual(model.placeholder, self.placeholder)
     
     def test_initalization_priority(self):
         """Test that input is preferred over the Json file."""
         
         model = DINCAE_like(self.model_params,
                                    n_channels = self.n_channels + 1,
-                                   image_nrows= self.nrows + 1,
-                                   image_ncols= self.ncols + 1,
                                    middle_channels = self.middle_channels + [1],
                                    kernel_sizes = self.kernel_sizes + [1],
                                    pooling_sizes = self.pooling_sizes + [1],
                                    interp_mode = "bilinear")
-        model.layers_setup()
         # Check that the network has the correct attributes
         self.assertEqual(model.n_channels, self.n_channels + 1)
-        self.assertEqual(model.image_ncols, self.ncols + 1)
         self.assertEqual(model.middle_channels, self.middle_channels + [1])
         self.assertEqual(model.kernel_sizes, self.kernel_sizes + [1])
         self.assertEqual(model.pooling_sizes, self.pooling_sizes + [1])
@@ -92,21 +84,22 @@ class Test_DINCAE_model(unittest.TestCase):
     def test_forward_pass(self):
         """Test if the forward pass of the model works correctly"""
         # Test the forward pass of the model
-        output = self.model(self.input_tensor, self.masks)
+        model = DINCAE_like()
+        input_tensor = th.rand(32, 13, 168, 144)
+        masks = (th.rand(32, 13, 168, 144) > 0.5).bool()  # Random masks
+        output = model(input_tensor, masks)
         
         # Check if the output shape is correct
-        self.assertEqual(output.shape, (self.batch_size, self.model.n_channels, self.nrows, self.ncols))
+        self.assertEqual(output.shape, (32, 2, 168, 144))
     
     def test_automatic_initialization(self):
-        channels_to_keep = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"]
         dataset_params = {
             "dataset": {
                 "cutted_nrows": self.nrows + 1,
                 "cutted_ncols": self.ncols + 1,
                 "dataset_kind": "test",
                 "test": {
-                    "n_channels": self.n_channels + 1,
-                    "channels_to_keep": channels_to_keep
+                    "n_channels": self.n_channels + 1
                 }
             }
         }
@@ -116,8 +109,6 @@ class Test_DINCAE_model(unittest.TestCase):
         self.assertIsInstance(model, DINCAE_like)
         self.assertEqual(dataset_kind, "extended")
         self.assertEqual(model.n_channels, self.n_channels + 1)
-        self.assertEqual(model.image_nrows, self.nrows + 1)
-        self.assertEqual(model.image_ncols, self.ncols + 1)
         self.assertEqual(model.middle_channels, self.middle_channels)
         self.assertEqual(model.kernel_sizes, self.kernel_sizes)
         self.assertEqual(model.pooling_sizes, self.pooling_sizes)
