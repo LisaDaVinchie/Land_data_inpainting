e538620bc7e970829f632541d798225977d08ec7
LisaDaVinchie <lisadavinchie@gmail.com>
2025-06-19 13:27:36 +0200

    Renamed

--- src/train.py
+++ src/train.py
@@ -1,38 +1,39 @@
+###############################################################################
+##  
+##  As the original training, but now the dataset is supposed to be normalized
+##  to the range [-1, 1] and the output is assumed to be normalized in the
+##  same range.
+##  
+###############################################################################
+
+
 import torch as th
 import torch.optim as optim
 import torch.nn.utils as utils
 from pathlib import Path
-import argparse
 from time import time
 import json
 
 from models import initialize_model_and_dataset_kind
-from losses import get_loss_function, calculate_valid_pixels
+from losses import get_loss_function
+from select_lr_scheduler import select_lr_scheduler
+from utils import change_dataset_idx, parse_params
 from CustomDataset_v2 import CreateDataloaders
 
 def main():
     """Main function to train a model on a dataset."""
     start_time = time()
     
-    parser = argparse.ArgumentParser(description='Train a CNN model on a dataset')
-    parser.add_argument('--paths', type=Path, help='Path to the JSON file with data paths')
-    parser.add_argument('--params', type=Path, help='Path to the JSON file with model parameters')
+    params, paths = parse_params()
     
-    args = parser.parse_args()
-    
-    params_file_path = Path(args.params)
-    paths_file_path = Path(args.paths)
-    if not params_file_path.exists():
-        raise FileNotFoundError(f"Parameters file not found: {params_file_path}")
-    if not paths_file_path.exists():
-        raise FileNotFoundError(f"Paths file not found: {paths_file_path}")
-    with open(params_file_path, 'r') as f:
-        params = json.load(f)
-    with open(paths_file_path, 'r') as f:
-        paths = json.load(f)
-    
-    train_perc = float(params["training"]["train_perc"])
-    batch_size = int(params["training"]["batch_size"])
+    training_params = params["training"]
+    train_perc = float(training_params["train_perc"])
+    batch_size = int(training_params["batch_size"])
+    epochs = int(training_params["epochs"])
+    model_kind = str(training_params["model_kind"])
+    learning_rate = float(training_params["learning_rate"])
+    loss_kind = str(training_params["loss_kind"])
+    scheduler_kind = str(training_params["lr_scheduler"])
     
     weights_path, results_path = configure_file_paths(paths)
     
@@ -57,10 +58,24 @@ def main():
     dl = CreateDataloaders(train_perc, batch_size)
     dataset = dl.load_dataset(dataset_path)
     train_loader, test_loader = dl.create(dataset)
-        
-    train = TrainModel(params, weights_path, results_path, dataset_specs)
     
-    train.train(train_loader, test_loader)
+    model, _ = initialize_model_and_dataset_kind(params, model_kind)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-8)
+    loss_function = get_loss_function(loss_kind)
+    print(f"Using loss function: {loss_function.__class__.__name__}", flush=True)
+    lr_scheduler = select_lr_scheduler(params, scheduler_kind, optimizer)
+        
+    train = TrainModel(model = model, 
+                      loss_function = loss_function, 
+                      optimizer = optimizer,
+                      lr_scheduler = lr_scheduler)
+    
+    train.results_path = results_path
+    train.weights_path = weights_path
+    train.params = params
+    train.dataset_specs = dataset_specs
+    
+    train.train(train_loader, test_loader, epochs)
     
     elapsed_time = time() - start_time
     print(flush=True)
@@ -93,35 +108,9 @@ def configure_file_paths(paths):
             raise FileNotFoundError(f"Directory {path.parent} does not exist")
     
     return weights_path, results_path
-
-def change_dataset_idx(dataset_path: Path, dataset_specs_path: Path, new_idx: int) -> tuple:
-    """Change the dataset index in the file names.
-
-    Args:
-        dataset_path (Path): latest dataset path
-        dataset_specs_path (Path): latest dataset specs path
-        new_idx (int): new dataset index
-
-    Raises:
-        FileNotFoundError: dataset file not found
-        FileNotFoundError: dataset specs file not found
-
-    Returns:
-        tuple: new dataset path, new dataset specs path
-    """
-    dataset_ext = dataset_path.suffix
-    dataset_name = dataset_path.stem.split("_")[0]
-    new_dataset_path = dataset_path.parent / f"{dataset_name}_{new_idx}{dataset_ext}"
-    
-    dataset_specs_ext = dataset_specs_path.suffix
-    dataset_specs_name = "dataset_specs"
-    
-    new_dataset_specs_path = dataset_specs_path.parent / f"{dataset_specs_name}_{new_idx}{dataset_specs_ext}"
-    
-    return new_dataset_path, new_dataset_specs_path
     
 class TrainModel:
-    def __init__(self, training_params, weights_path, results_path, dataset_specs = None):
+    def __init__(self, model, loss_function, optimizer, clip_value = 5.0, lr_scheduler = None, save_every = 1):
         """Initialize the training class.
 
         Args:
@@ -130,68 +119,26 @@ class TrainModel:
             results_path (_type_): _description_
             dataset_specs (_type_, optional): _description_. Defaults to None.
         """
-        self.params = training_params
+
         self.device = th.device("cuda" if th.cuda.is_available() else "cpu")
         
-        self.weights_path = weights_path
-        self.results_path = results_path
-        self.dataset_specs = dataset_specs
+        self.model = model.to(self.device)
+        self.loss_function = loss_function
+        self.optimizer = optimizer
+        self.scheduler = lr_scheduler
+        self.clip_value = clip_value
+        self.save_every = save_every
         
-        self._configure_training_parameters()
-        self.nan_placeholder = float(dataset_specs["dataset"]["nan_placeholder"])
-        
-        
-        self._initialize_training_components()
+        self.results_path = None
+        self.weights_path = None
+        self.params = None
+        self.dataset_specs = None
         
         self.train_losses = []
         self.test_losses = []
         self.training_lr = []
-
-    def _configure_training_parameters(self):
-        training_params = self.params["training"]
         
-        self.model_kind = training_params["model_kind"]
-        
-        self.loss_kind = str(training_params["loss_kind"])
-        self.lr = training_params['learning_rate']
-        self.lr_scheduler = training_params["lr_scheduler"]
-        self.epochs = training_params['epochs']
-        
-        self.save_every = training_params['save_every']
-        
-        if self.save_every <= 0:
-            raise ValueError("Save interval must be positive.")
-        
-        self.clip_value = training_params["clip_value"]
-
-    def _initialize_training_components(self, lr = None, lr_scheduler = None, loss_kind = None, nan_placeholder = None, model_kind = None):
-        
-        lr = lr if lr is not None else self.lr
-        lr_scheduler = lr_scheduler if lr_scheduler is not None else self.lr_scheduler
-        loss_kind = loss_kind if loss_kind is not None else self.loss_kind
-        nan_placeholder = nan_placeholder if nan_placeholder is not None else self.nan_placeholder
-        model_kind = model_kind if model_kind is not None else self.model_kind
-        
-        self.model, self.dataset_kind = initialize_model_and_dataset_kind(self.params, model_kind, self.dataset_specs)
-        
-        self.loss_function = get_loss_function(loss_kind, nan_placeholder)
-        
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lr, betas=(0.9, 0.999), eps=1e-8)
-        
-        self.scheduler = None
-        if lr_scheduler == "step":
-            step_size = int(self.params["lr_schedulers"][lr_scheduler]["step_size"])
-            gamma = float(self.params["lr_schedulers"][lr_scheduler]["gamma"])
-            self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=step_size, gamma=gamma)
-        if lr_scheduler == "lambda":
-            factor = float(self.params["lr_schedulers"][lr_scheduler]["factor"])
-            step_size = int(self.params["lr_schedulers"][lr_scheduler]["step_size"])
-            lr_lambda = lambda step: factor ** -(step // step_size)
-            self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lr_lambda)
-        elif lr_scheduler != "none":
-            raise ValueError(f"Unknown lr_scheduler: {lr_scheduler}")
-        
-    def train(self, train_loader: th.utils.data.DataLoader, test_loader: th.utils.data.DataLoader, epochs: int = None):
+    def train(self, train_loader: th.utils.data.DataLoader, test_loader: th.utils.data.DataLoader, epochs: int):
         """Train the model on the dataset.
 
         Args:
@@ -199,54 +146,71 @@ class TrainModel:
             test_loader (th.utils.data.DataLoader): testing dataloader
         """
         
-        epochs = epochs if epochs is not None else self.epochs
-        
-        if self.epochs <= 0:
-            raise ValueError("Number of epochs must be positive.")
-        
         print(flush=True)
         for epoch in range(epochs):
             print(f"Epoch {epoch + 1}/{epochs}\n", flush=True)
             self.model.train()
-            epoch_loss = 0.0
-            n_valid_pixels = 0
-            for (images, masks) in train_loader:
-                loss = self._compute_loss(images, masks)
-                epoch_loss += loss.item()
-                
-                loss.backward()
-                utils.clip_grad_value_(self.model.parameters(), self.clip_value)  # Clip gradients to avoid exploding gradients
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-                n_valid_pixels += calculate_valid_pixels(masks[:, 4], images[:, 4], self.nan_placeholder)
+            total_train_loss = self.train_step(train_loader)
 
             self.training_lr.append(self.optimizer.param_groups[0]['lr'])
             self.scheduler.step() if self.scheduler is not None else None
             
-            self.train_losses.append(epoch_loss / (n_valid_pixels + 1e-8))
+            self.train_losses.append(total_train_loss)
             
             with th.no_grad():
                 self.model.eval()
-                epoch_loss = 0.0
-                n_valid_pixels = 0
-                for (images, masks) in test_loader:
-                    loss = self._compute_loss(images, masks)
-                    epoch_loss += loss.item()
-                    n_valid_pixels += calculate_valid_pixels(masks[:, 4], images[:, 4], self.nan_placeholder)
-                self.test_losses.append(epoch_loss / (n_valid_pixels + 1e-8))
+                total_test_loss = self.train_step(test_loader, backpropagate=False)
+            self.test_losses.append(total_test_loss)
             
             if (epoch + 1) % self.save_every == 0:
                 self.save_weights()
                 self.save_results()
-                print(f"\nModel weights and results saved at epoch {epoch + 1}\n", flush=True)
+                print(f"\nModel weights and results saved at epoch {epoch + 1} at path {self.weights_path}\n", flush=True)
                 
         print(flush=True)
+
+    def train_step(self, dataloader, backpropagate=True):
+        epoch_loss: float = 0.0
+        n_valid_pixels: int = 0
+        for (images, masks, nan_masks) in dataloader:
+            # loss = self._compute_loss(images, masks, nan_masks)
+            # Multiply images by masks to exxlude information from masked pixels
+            output = self.model(images * masks.float(), (masks & nan_masks).float())
+            loss = self.loss_function(output[:, 0], images[:, 4], self.validation_mask(masks[:, 4], nan_masks[:, 4]))
+            epoch_loss += loss.item()
+            if backpropagate:
+                loss.backward()
+                utils.clip_grad_value_(self.model.parameters(), self.clip_value)  # Clip gradients to avoid exploding gradients
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+            
+            n_valid_pixels += self.calculate_valid_pixels(masks[:, 4], nan_masks[:, 4])
+
+        return epoch_loss / (n_valid_pixels + 1e-8)  # Avoid division by zero
+    
+    def validation_mask(self, masks: th.Tensor, nan_masks: th.Tensor, loss: bool = True):
+        """Calculate the mask used to calculate the loss, i.e. where the pixel is masked but not nan.
+
+        Args:
+            masks (th.Tensor): masks tensor
+            nan_masks (th.Tensor): nan masks tensor
+
+        Returns:
+            th.Tensor: validation mask
+        """
+        return ~(~masks & nan_masks) if loss else (~masks & nan_masks)
+    
+    def calculate_valid_pixels(self, masks: th.Tensor, nan_masks: th.Tensor):
+        return self.validation_mask(masks, nan_masks, loss=False).float().sum().item()
                 
-    def _compute_loss(self, images, masks):
+    def _compute_loss(self, images, masks, nan_masks):
         images = images.to(self.device)
         masks = masks.to(self.device)
-        output = self.model(images, masks.float())
-        loss = self.loss_function(output[:, 0], images[:, 4], masks[:, 4])
+        nan_masks = nan_masks.to(self.device)
+        
+        # Multiply images by masks to exxlude information from masked pixels
+        output = self.model(images * masks.float(), (masks & nan_masks).float())
+        loss = self.loss_function(output[:, 0], images[:, 4], self.validation_mask(masks[:, 4], nan_masks[:, 4]))
         return loss
     
     def save_weights(self):
