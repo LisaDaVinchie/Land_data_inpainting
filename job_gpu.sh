f9ec2e012124c3eee1ce3381047dc4ff68f3454d
LisaDaVinchie <lisadavinchie@gmail.com>
2025-09-09 01:26:29 +0200

    fix

--- job_gpu.sh
+++ job_gpu.sh
@@ -12,8 +12,8 @@ source bot_codes.txt
 bot_id=${bot_id}
 chat_id=${chat_id}
 
-source wandb_api.txt
-wandb_api=${api_code}
+# source wandb_api.txt
+# wandb_api=${api_code}
 
 export WANDB_API_KEY=${wandb_api}
 
