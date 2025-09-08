bf43e1ffe8ec0ac704e66fe9f69d151001323132
LisaDaVinchie <lisadavinchie@gmail.com>
2025-09-08 15:44:53 +0200

    removed models

--- src/models.py
+++ src/models.py
@@ -10,123 +10,8 @@ def conv_output_size_same_padding(in_size, pool_size):
 def conv_output_size(in_size, kernel, padding, stride):
     return  math.ceil((in_size - kernel + 2 * padding) / stride + 1)
 
-
-n_channels_string = "n_channels"
-image_nrows_string = "cutted_nrows"
-image_ncols_string = "cutted_ncols"
-
-model_cathegory_string: str = "models"
-dataset_cathegory_string: str = "dataset"
-
-def get_model_class(params, model_kind: str, n_channels: int = 13) -> tuple[nn.Module, str]:
-    """Initialize the model and dataset kind from the json file.
-
-    Args:
-        params: json file with parameters
-        model_kind (str): kind of model to initialize
-
-    Raises:
-        ValueError: if the model kind is not recognized
-
-    Returns:
-        tuple[nn.Module, str]: model and dataset kind
-    """
-
-    if model_kind == "simple_conv":
-        model = simple_conv(params)
-    elif model_kind == "DINCAE_like":
-        model = DINCAE_like(n_channels=n_channels)
-    elif model_kind == "DINCAE_pconvs":
-        model = DINCAE_pconvs(n_channels=n_channels)
-    elif model_kind == "dummy":
-        model = DummyModel(n_channels=n_channels)
-    elif model_kind == "dummier":
-        model = DummierModel(n_channels=n_channels)
-    else:
-        raise ValueError(f"Model kind {model_kind} not recognized")
-    
-    # if dataset_params is not None:
-    #     model.override_load_dataset_configurations(dataset_params)
-    
-    return model
-
 def mask_image(images, masks, placeholder):
     return th.where(masks.bool(), images, placeholder * th.ones_like(images))
-class DummierModel(nn.Module):
-    def __init__(self, params = None, n_channels: int = 13, total_days: int = 9):
-        """Dummy model for testing purposes. Returns the mean of the previous and following days.
-
-        Args:
-            params (_type_, optional): _description_. Defaults to None.
-            n_channels (int, optional): _description_. Defaults to 13.
-            total_days (int, optional): _description_. Defaults to 9.
-        """
-        super(DummierModel, self).__init__()
-        
-        self.n_channels = n_channels
-        self.total_days = total_days
-        
-        self.layers_setup()
-    
-    def forward(self, images: th.Tensor, masks: th.Tensor) -> th.Tensor:
-        
-        B, _, H, W = images.shape
-        
-        return th.ones((B, 2, H, W), dtype=th.float32, requires_grad = True) * 4
-    
-    def layers_setup(self):
-        """Dummy method to satisfy the interface."""
-        self.conv1 = nn.Conv2d(2, 2, kernel_size=3, padding=1)
-        
-    
-    def override_load_dataset_configurations(self, params):
-        pass
-
-class DummyModel(nn.Module):
-    def __init__(self, params = None, n_channels: int = 13, total_days: int = 9):
-        """Dummy model for testing purposes. Returns the mean of the previous and following days.
-
-        Args:
-            params (_type_, optional): _description_. Defaults to None.
-            n_channels (int, optional): _description_. Defaults to 13.
-            total_days (int, optional): _description_. Defaults to 9.
-        """
-        super(DummyModel, self).__init__()
-        
-        self.n_channels = n_channels
-        self.total_days = total_days
-        self.layers_setup()
-        # Add a dummy parameter that does nothing but enables gradient clipping
-    
-    def forward(self, images: th.Tensor, masks: th.Tensor) -> th.Tensor:
-        # c = self.total_days // 2
-
-        # # Select all the channels of the SST except the one to predict
-        # known_channels = range(self.total_days)
-        # known_channels = [i for i in known_channels if i != c]
-        self.dummy_param = nn.Parameter(th.zeros(1), requires_grad=True)
-        
-        c = 4
-        known_channels = [0, 1, 2, 3, 5, 6, 7, 8]
-        
-        known_images = images[:, known_channels, :, :]
-        
-        known_images = th.where(masks[:, known_channels, :, :].bool(), known_images, th.nan)
-        
-        mean_image = th.nanmean(known_images, dim=1, keepdim=True)
-        dummystd = th.zeros_like(mean_image)
-        mean_image = th.where(masks[:, c:c+1, :, :].bool(), images[:, c:c+1, :, :], mean_image)
-        mean_image = th.cat([mean_image, dummystd], dim=1) * (1 + self.dummy_param)
-        
-        return mean_image
-    
-    def layers_setup(self):
-        """Dummy method to satisfy the interface."""
-        self.conv1 = nn.Conv2d(2, 2, kernel_size=3, padding=1)
-        
-    
-    def override_load_dataset_configurations(self, params):
-        pass
 
 class DINCAE_pconvs(nn.Module):
     def __init__(self, n_channels: int = 13, middle_channels: List[int] = [16, 30, 58, 110, 209], kernel_sizes: List[int] = [3, 3, 3, 3, 3], pooling_sizes: List[int] = [2, 2, 2, 2, 2], interp_mode: str = "bilinear"):
@@ -163,32 +48,8 @@ class DINCAE_pconvs(nn.Module):
     def print_shapes(self, layer_name, x):
         if self.print:
             print(f"{layer_name} shape: {x.shape}", flush=True)
-
-    def _load_model_configurations(self, params):
-        if params is not None:
-            model_params = params[model_cathegory_string].get(self.model_name, None)
-            if self.middle_channels is None:
-                self.middle_channels = model_params.get("middle_channels", None)
-            if self.kernel_sizes is None:
-                self.kernel_sizes = model_params.get("kernel_sizes", None)
-            if self.pooling_sizes is None:
-                self.pooling_sizes = model_params.get("pooling_sizes", None)
-            if self.interp_mode is None:
-                self.interp_mode = model_params.get("interp_mode", None)
-            
-        for var in [self.middle_channels, self.kernel_sizes, self.pooling_sizes, self.interp_mode]:
-            if var is None:
-                raise ValueError(f"Variable {var} is None. Please provide a value for it.")
     
-    def override_load_dataset_configurations(self, params):
-        if params is not None:
-            dataset_kind = params[dataset_cathegory_string].get("dataset_kind", None)
-            channels_to_keep = params[dataset_cathegory_string][dataset_kind].get("channels_to_keep", None)
-            self.n_channels = len(channels_to_keep)
-        if self.n_channels is None:
-            raise ValueError(f"Variable n_channels is None. Please provide a value for it.")
-    
-    def forward(self, x: th.Tensor, mask: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
+    def forward(self, x: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
         """Forward pass
 
         Args:
@@ -199,6 +60,9 @@ class DINCAE_pconvs(nn.Module):
             th.Tensor: output image and mask
         """
         
+        mask = th.isnan(x)
+        x = th.nan_to_num(x, nan=-2)
+
         self.print_shapes("input", x)
         x1, mask1 = self.enc1(x, mask)
         self.print_shapes("enc1", x1)
@@ -257,13 +121,7 @@ class DecoderBlock(nn.Module):
         x = self.activation(x)
         if e_x is not None and e_mask is not None:
             x = x + e_x
-            # mask = mask * e_mask
-            mask = th.clamp(mask + e_mask, 0, 1)
-            # mask = mask + e_mask
-            
-            # x = th.cat([x, e_x], dim=1)
-            # mask = th.cat([mask, e_mask], dim=1)
-            
+            mask = th.clamp(mask + e_mask, 0, 1)   
         
         return x, mask.float()
 
@@ -307,28 +165,6 @@ class DINCAE_like(nn.Module):
     def print_shapes(self, layer_name, x):
         if self.print:
             print(f"{layer_name} shape: {x.shape}", flush=True)
-            
-    def _load_model_configurations(self, params):
-        model_params = params[model_cathegory_string].get(self.model_name, None)
-        if self.middle_channels is None:
-            self.middle_channels = model_params.get("middle_channels", None)
-        if self.kernel_sizes is None:
-            self.kernel_sizes = model_params.get("kernel_sizes", None)
-        if self.pooling_sizes is None:
-            self.pooling_sizes = model_params.get("pooling_sizes", None)
-        if self.interp_mode is None:
-            self.interp_mode = model_params.get("interp_mode", None)
-        
-        for var in [self.middle_channels, self.kernel_sizes, self.pooling_sizes, self.interp_mode]:
-            if var is None:
-                raise ValueError(f"Variable {var} is None. Please provide a value for it.")
-    
-    def override_load_dataset_configurations(self, params):
-        if params is not None:
-            dataset_kind = params[dataset_cathegory_string].get("dataset_kind", None)
-            self.n_channels = params[dataset_cathegory_string][dataset_kind].get(n_channels_string, None)
-        if self.n_channels is None:
-            raise ValueError(f"Variable n_channels is None. Please provide a value for it.")
         
     def forward(self, images: th.Tensor, masks: th.Tensor) -> th.Tensor:
         """Forward pass
@@ -441,37 +277,6 @@ class simple_conv(nn.Module):
             nn.ConvTranspose2d(self.middle_channels[0], self.n_channels, kernel_size=self.kernel_size[0], stride=self.stride[0], padding=self.padding[2], output_padding=self.output_padding[2]),  # 32x32 -> 64x64
             nn.Sigmoid()  # Output between 0 and 1
         )
-        
-    def _load_model_configurations(self, params):
-        if params is not None:
-            self.middle_channels = params[model_cathegory_string][self.model_name].get("middle_channels", None)
-            self.kernel_size = params[model_cathegory_string][self.model_name].get("kernel_size", None)
-            self.stride = params[model_cathegory_string][self.model_name].get("stride", None)
-            self.padding = params[model_cathegory_string][self.model_name].get("padding", None)
-            self.output_padding = params[model_cathegory_string][self.model_name].get("output_padding", None)
-        
-        for var in [self.middle_channels, self.kernel_size, self.stride, self.padding, self.output_padding]:
-            if var is None:
-                raise ValueError(f"Variable {var} is None. Please provide a value for it.")
-    
-    def _load_dataset_configurations(self, params):
-        if params is not None:
-            dataset_kind = params[dataset_cathegory_string].get("dataset_kind", None)
-            channels_to_keep = params[dataset_cathegory_string][dataset_kind].get("channels_to_keep", None)
-            self.n_channels = len(channels_to_keep) + 1
-        
-        if self.n_channels is None:
-            raise ValueError(f"Variable {self.n_channels} is None. Please provide a value for it.")
-    
-    def override_load_dataset_configurations(self, params):
-        self.n_channels = None
-        if params is not None:
-            dataset_kind = params[dataset_cathegory_string].get("dataset_kind", None)
-            channels_to_keep = params[dataset_cathegory_string][dataset_kind].get("channels_to_keep", None)
-            self.n_channels = len(channels_to_keep) + 1
-            
-        if self.n_channels is None:
-            raise ValueError(f"Variable {self.n_channels} is None. Please provide a value for it.")
         
     def forward(self, x: th.Tensor, masks: th.Tensor) -> th.Tensor:
         images = mask_image(x, masks, self.placeholder)
